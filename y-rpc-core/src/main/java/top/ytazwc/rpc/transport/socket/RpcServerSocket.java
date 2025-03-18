c730a7db1b576949e6b8d04688eedfd4a8c164ea
花木凋零成兰 <18570354653@163.com>
2025-03-18 23:02:59 +0800

    基于socket实现rpc服务

---
+++ y-rpc-core/src/main/java/top/ytazwc/rpc/transport/socket/RpcServerSocket.java
@@ -0,0 +1,14 @@
+package top.ytazwc.rpc.transport.socket;
+
+/**
+ * @author 花木凋零成兰
+ * @title RpcServerSocket
+ * @date 2025-03-18 22:24
+ * @package top.ytazwc.rpc.transport.socket
+ * @description 基于socket实现的rpc服务端
+ */
+public class RpcServerSocket {
+
+
+
+}
