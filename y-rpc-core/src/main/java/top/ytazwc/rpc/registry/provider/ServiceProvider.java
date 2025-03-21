99654a0da757971681cd60088b30ee1b540b75ad
yangtao <00103943@lesso.com>
2025-03-21 17:44:53 +0800

    基于zk的服务提供者

--- y-rpc-core/src/main/java/top/ytazwc/rpc/registry/provider/ServiceProvider.java
+++ y-rpc-core/src/main/java/top/ytazwc/rpc/registry/provider/ServiceProvider.java
@@ -11,20 +11,20 @@ import top.ytazwc.rpc.config.RpcServiceConfig;
 public interface ServiceProvider {
 
     /**
-     *
+     * 新增服务 缓存
      * @param config 服务配置信息
      */
     void addService(RpcServiceConfig config);
 
     /**
-     * 获取调用服务
+     * 从缓存中获取服务实例
      * @param rpcServiceName 服务唯一标识
      * @return 返回服务实例
      */
     Object getService(String rpcServiceName);
 
     /**
-     *
+     * 注册服务
      * @param config 服务配置信息
      */
     void publishService(RpcServiceConfig config);
--- y-rpc-core/src/main/java/top/ytazwc/rpc/registry/provider/impl/ZkServiceProviderImpl.java
+++ y-rpc-core/src/main/java/top/ytazwc/rpc/registry/provider/impl/ZkServiceProviderImpl.java
@@ -1,28 +1,81 @@
 package top.ytazwc.rpc.registry.provider.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import top.ytazwc.rpc.config.RpcServiceConfig;
+import top.ytazwc.rpc.enums.RpcErrorMessage;
+import top.ytazwc.rpc.exception.RpcException;
+import top.ytazwc.rpc.registry.ServiceRegistry;
 import top.ytazwc.rpc.registry.provider.ServiceProvider;
+import top.ytazwc.rpc.registry.zk.ZkServiceRegistryImpl;
+import top.ytazwc.rpc.utils.factory.SingletonFactory;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static top.ytazwc.rpc.constant.RpcConstant.PORT;
 
 /**
  * @author 00103943
  * @date 2025-03-19 14:11
  * @package top.ytazwc.rpc.registry.provider
- * @Description TODO
+ * @Description 服务提供者 调用进行服务注册接口实现 基于Zookeeper
  */
+@Slf4j
 public class ZkServiceProviderImpl implements ServiceProvider {
+
+    // 缓存 service 服务实现类 interfaceName + version + group ： obj
+    private final Map<String, Object> serviceMap;
+
+    // 注册服务集合 避免重复
+    private final Set<String> registeredService;
+
+    // 服务注册接口 调用该接口进行服务注册
+    private final ServiceRegistry registry;
+
+    public ZkServiceProviderImpl() {
+
+        serviceMap = new ConcurrentHashMap<>();
+        registeredService = ConcurrentHashMap.newKeySet();
+        registry = SingletonFactory.getInstance(ZkServiceRegistryImpl.class);
+
+    }
 
     @Override
     public void addService(RpcServiceConfig config) {
-
+        // 获取服务名
+        String serviceName = config.getRpcServiceName();
+        // 服务已经注册过 则不重复增加
+        if (registeredService.contains(serviceName)) {
+            return ;
+        }
+        registeredService.add(serviceName);
+        // 缓存实例
+        serviceMap.put(serviceName, config.getService());
+        log.info("新注册服务: [{}] 接口为: [{}]", serviceName, config.getService().getClass().getInterfaces());
     }
 
     @Override
     public Object getService(String rpcServiceName) {
-        return null;
+        Object service = serviceMap.get(rpcServiceName);
+        if (Objects.isNull(service)) {
+            throw new RpcException(RpcErrorMessage.SERVICE_NOT_FOUND);
+        }
+        return service;
     }
 
     @Override
     public void publishService(RpcServiceConfig config) {
-
+        try {
+            String host = InetAddress.getLocalHost().getHostAddress();
+            this.addService(config);
+            registry.registryService(config.getRpcServiceName(), new InetSocketAddress(host, PORT));
+        } catch (UnknownHostException e) {
+            log.error("获取本机域名失败!!!", e);
+        }
     }
 }
