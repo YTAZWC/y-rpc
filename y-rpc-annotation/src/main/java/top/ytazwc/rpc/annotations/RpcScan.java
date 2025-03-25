d78d1353efbccfdfc85657f88e885fa87110afa9
yangtao <00103943@lesso.com>
2025-03-25 17:47:49 +0800

    自定义注解扫描需要注册的自定义bean注解

--- y-rpc-annotation/src/main/java/top/ytazwc/rpc/annotations/RpcScan.java
+++ y-rpc-annotation/src/main/java/top/ytazwc/rpc/annotations/RpcScan.java
@@ -1,11 +1,19 @@
 package top.ytazwc.rpc.annotations;
 
+import org.springframework.context.annotation.Import;
+import top.ytazwc.rpc.spring.RpcRegistrar;
+
+import java.lang.annotation.*;
+
 /**
  * @author 00103943
  * @date 2025-03-25 17:40
  * @package top.ytazwc.rpc.annotations
  * @description 自定义bean扫描注解
  */
-
+@Documented
+@Target({ElementType.TYPE})
+@Import({RpcRegistrar.class})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RpcScan {
 }
