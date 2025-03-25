866cd89227d17991bbe89ebd4db243930b2c5e00
yangtao <00103943@lesso.com>
2025-03-25 17:44:38 +0800

    自定义注解扫描需要注册的自定义bean注解

---
+++ y-rpc-annotation/src/main/java/top/ytazwc/rpc/annotations/RpcScan.java
@@ -0,0 +1,11 @@
+package top.ytazwc.rpc.annotations;
+
+/**
+ * @author 00103943
+ * @date 2025-03-25 17:40
+ * @package top.ytazwc.rpc.annotations
+ * @description 自定义bean扫描注解
+ */
+
+public @interface RpcScan {
+}
---
+++ y-rpc-annotation/src/main/java/top/ytazwc/rpc/spring/RpcRegistrar.java
@@ -0,0 +1,28 @@
+package top.ytazwc.rpc.spring;
+
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.context.ResourceLoaderAware;
+import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.type.AnnotationMetadata;
+
+/**
+ * @author 00103943
+ * @date 2025-03-25 17:42
+ * @package top.ytazwc.rpc.spring
+ * @description
+ */
+public class RpcRegistrar implements ImportBeanDefinitionRegistrar, ResourceLoaderAware {
+
+    @Override
+    public void setResourceLoader(ResourceLoader resourceLoader) {
+
+    }
+
+    @Override
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry, BeanNameGenerator importBeanNameGenerator) {
+        ImportBeanDefinitionRegistrar.super.registerBeanDefinitions(importingClassMetadata, registry, importBeanNameGenerator);
+    }
+
+}
