b3c9c4c9d12d74a94c0a153611df9055c7b22399
yangtao <00103943@lesso.com>
2025-03-26 17:47:56 +0800

    测试 kryo 序列化 + gzip 压缩算法

--- y-rpc-core/src/test/java/top/ytazwc/rpc/SerializerTests.java
+++ y-rpc-core/src/test/java/top/ytazwc/rpc/SerializerTests.java
@@ -1,6 +1,8 @@
 package top.ytazwc.rpc;
 
 import org.junit.Test;
+import top.ytazwc.rpc.compress.Compress;
+import top.ytazwc.rpc.compress.gzip.GzipCompress;
 import top.ytazwc.rpc.entity.RpcRequest;
 import top.ytazwc.rpc.serializer.Serializer;
 import top.ytazwc.rpc.serializer.kryo.KryoSerializer;
@@ -23,15 +25,26 @@ public class SerializerTests {
     @Test
     public void testKryoSerializer() throws IOException {
         Serializer serializer = new KryoSerializer();
+        Compress compress = new GzipCompress();
         TestService testService = new TestServiceImpl();
-        RpcRequest request = RpcRequest.builder()
-                .version("version")
-                .group("group")
+//        RpcRequest request = RpcRequest.builder()
+//                .version("version")
+//                .group("group")
+//                .requestId(UUID.randomUUID().toString())
+//                .interfaceName(testService.getClass().getInterfaces()[0].getName())
+//                .paramTypes(new Class[]{String.class, String.class})
+//                .parameters(new Object[]{"String", "String"})
+//                .build();
+
+        RpcRequest request = RpcRequest.builder().methodName("hello")
+                .parameters(new Object[]{"sayheloossssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssloo", "sayhelooloosayhelooloo"})
+                .interfaceName("top.ytazwc.rpc")
+                .paramTypes(new Class<?>[]{String.class, String.class})
                 .requestId(UUID.randomUUID().toString())
-                .interfaceName(testService.getClass().getInterfaces()[0].getName())
-                .paramTypes(new Class[]{})
-                .parameters(new Object[]{})
+                .group("group1")
+                .version("version1")
                 .build();
+
         // Java 自带序列化
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         ObjectOutputStream outputStream = new ObjectOutputStream(out);
@@ -42,14 +55,15 @@ public class SerializerTests {
         System.out.println(request);
 
         // 序列化之后 数据大小
-        System.out.println("序列化之后, 数据大小: " + byteArray.length);
+        System.out.println("Java 序列化之后, 数据大小: " + byteArray.length);
 
         // 进行序列化
         byte[] kryoByte = serializer.serialize(request);
-
+        // 对 kryo 序列化之后数据 进一步压缩
+        byte[] gzipByte = compress.compress(kryoByte);
         // kryo 序列化之后 数据大小
         System.out.println("kryo 序列化之后, 数据大小: " + kryoByte.length);
-
+        System.out.println("进一步压缩后数据大小: " + gzipByte.length);
         // 反序列化结果
         RpcRequest deserialize = serializer.deserialize(kryoByte, RpcRequest.class);
         System.out.println(deserialize);
